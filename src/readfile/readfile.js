996c152ce6b57bb38f7cbb4bd8978e13099aa237
Nguyen Duc Thang <ndthang0000@gmail.com>
2023-04-03 15:23:15 +0700

    update upload file word and read word file

--- src/readfile/readfile.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const readFile = async (name) => {
-  const data = fs.readFileSync(`uploads/${name}`, { encoding: 'utf-8' });
-  console.log(data);
-  console.log('vô đây');
-};
-readFile('test.docx');

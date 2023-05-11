d539938cc0453d9a7fb5b157676e2311fce13ec5
TheOriginal <whynotmemylove99@gmail.com>
2023-05-11 15:15:10 +0100

    updatefunction

--- src/python/translatorDocx.py
+++ src/python/translatorDocx.py
@@ -2,6 +2,8 @@
 #pip install deep-translator
 import docx
 from deep_translator import GoogleTranslator
+from underthesea import word_tokenize
+from underthesea import sent_tokenize
 #deep_translator dung gg API
 
 
@@ -17,7 +19,7 @@ from deep_translator import GoogleTranslator
 #langs_dict = GoogleTranslator().get_supported_languages(as_dict=True)  # output: {arabic: ar, french: fr, english:en etc...}
 #print(langs_dict)
 
-
+'''
 #-----------------------------------------------------------------------------------------
 # Mở file Docx gốc và file Docx mới : anylanguage! =)))
 docx_file = docx.Document(r"uploads\1683339435548-MSSV.docx")
@@ -34,34 +36,44 @@ for paragraph in docx_file.paragraphs:
 
 # Lưu trữ đối tượng Docx mới vào file Docx mới
 new_docx_file.save('uploads/new.docx')
+'''
+
+#---------------------------------------------------------
+#function 1: function tách câu từ file : input là tên file , output là array đã các câu được tách
+def sent_from_file(filename):
+    docx_file = docx.Document(r"uploads/"+filename)
+    #all_paras = docx_file.paragraphs
+    #print(len(all_paras))
+    sent = []
+    for paragraph in docx_file.paragraphs:
+        text = paragraph.text
+        sent_Vi = sent_tokenize(text)
+        sent = sent + sent_Vi
+    return sent 
+
+#---------------------------------------------------------
+#function 2: function dịch 1 câu: input là 1 câu; output là 1 câu đã đc dịch
+def translate_sent(sent):
+    translated_text = GoogleTranslator(source='auto', target='en').translate(sent)
+    return translated_text
+
+#---------------------------------------------------------
+#function 3: function dịch nhiều câu: input là array các câu chưa dịch, output là mảng các câu đc dịch
+def translate_sents(sents):
+    translated_sents = []
+    for sent in sents:
+        translated_sent = translate_sent(sent)
+        translated_sents.append(translated_sent)
+    return translated_sents
+#---------------------------------------------------------
 
 
-#------------------------------------------------------------------
-#for Pdf:
-#pip install PyPDF2
-import PyPDF2
-from deep_translator import GoogleTranslator
+#test
+filename = "vietnamtext.docx"
+sent = sent_from_file(filename)
+print(sent)
+print("-------------------------------------------------")
+translated_sent = translate_sents(sent)
+print(translated_sent)
 
-# Mở file PDF gốc và file PDF mới
-pdf_file = open(r'uploads\simplePdf.pdf', 'rb')
-new_pdf_file = open('uploads/new.pdf', 'wb')
 
-# Tạo đối tượng PDF và đối tượng trang mới
-pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-pdf_writer = PyPDF2.PdfFileWriter()
-
-# Dịch văn bản từ file PDF gốc sang tiếng Việt và lưu vào file PDF mới
-
-for page_num in range(pdf_reader.numPages):
-    page = pdf_reader.getPage(page_num)
-    text = page.extractText()
-    translated_text = GoogleTranslator(source='auto', target='vi').translate(text)
-    page.mergePage(PyPDF2.pdf.PageObject.createTextObject(None, translated_text))
-    pdf_writer.addPage(page)
-
-# Lưu trữ đối tượng PDF mới vào file PDF mới
-pdf_writer.write(new_pdf_file)
-
-# Đóng các file
-pdf_file.close()
-new_pdf_file.close()

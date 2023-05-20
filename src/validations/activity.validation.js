f1135ed36c3ada1811dcb2f38751c9a5cbc6cb02
LonggConn <zzbautroizz332000@gmail.com>
2023-05-20 10:15:05 +0700

    Update activity.validation.js

--- src/validations/activity.validation.js
+++ src/validations/activity.validation.js
@@ -17,7 +17,29 @@ const getActivity = {
   }),
 };
 
+const updateActivity = {
+  params: Joi.object().keys({
+    userId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+        comment: Joi.string(),
+        userId: Joi.string().required(),
+        action: Joi.string().required(),
+        projectId: Joi.string().required(),
+    })
+    .min(1),
+};
+
+const deleteActivity = {
+  params: Joi.object().keys({
+    userId: Joi.string().custom(objectId),
+  }),
+};
+
 module.exports = {
   getActivities,
   getActivity,
+  updateActivity,
+  deleteActivity,
 };

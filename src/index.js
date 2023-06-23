d7769820151b28b911a79c7bbc5639e44a2c18e2
Thien Tam <daothithientam@gmail.com>
2023-06-23 14:29:34 +0700

    auto create elastic index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,34 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
+const axios = require('axios');
 
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
+
+  axios
+    .put('http://localhost:9200/translationmemories')
+    .then((res) => {
+      if (res.acknowledged) {
+        logger.info('Create translationmemories index successfully');
+      }
+    })
+    .catch((error) => {
+      console.log('elastic', error.response.data.error.reason);
+    });
+
+  axios
+    .put('http://localhost:9200/term-base')
+    .then((res) => {
+      if (res.acknowledged) {
+        logger.info('Create term-base index successfully');
+      }
+    })
+    .catch((error) => {
+      console.log('elastic', error.response.data.error.reason);
+    });
+
   server = app.listen(config.port, () => {
     logger.info(`Listening to port ${config.port}`);
   });
